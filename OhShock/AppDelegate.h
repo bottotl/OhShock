18ebe5653baf90343461f6e92261241fa6c0fb0d
lintao.yu <lintao.yu@dianping.com>
2015-12-03 14:59:30 +0800

    重写项目
    引入 CocoaPods 。先写引导页面

---
+++ OhShock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OhShock
+//
+//  Created by Lintao.Yu on 12/3/15.
+//  Copyright © 2015 Lintao Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OhShock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OhShock
+//
+//  Created by Lintao.Yu on 12/3/15.
+//  Copyright © 2015 Lintao Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
