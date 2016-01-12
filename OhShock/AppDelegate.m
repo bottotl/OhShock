dc7d67d8493c6f3c2a706662d23006c140ce6f52
bottotl <377632523@qq.com>
2016-01-12 10:04:44 +0800

    merge commit

--- OhShock/AppDelegate.m
+++ OhShock/AppDelegate.m
@@ -15,12 +15,17 @@
 #import "LTMainTabBarController.h"
 #import "UIColor+expanded.h"
 #import "UIImage+Common.h"
+#import <AMapLocationKit/AMapLocationKit.h>
+#import <AMapSearchKit/AMapSearchKit.h>
+
 
 #warning for test
 #import "LTPostListViewController.h"
 
 #define kColorTableSectionBg [UIColor colorWithHexString:@"0xe5e5e5"]
 #define  kNavTitleFontSize 19
+#define GAODE_APPKEY @"cbf7020906739e6c33110f18bc6f261e" //高德地图key
+
 
 static NSString *const AppID = @"AeqpkvIfdCdKWr080LveKfEl";
 static NSString *const AppKey = @"UwgavmLDCILH6xr6P7gXob8J";
@@ -51,6 +56,14 @@ static NSString *const AppKey = @"UwgavmLDCILH6xr6P7gXob8J";
     /// 注册推送
     [self registerPush];
     
+    
+    //注册高德地图AppKey
+    [AMapLocationServices sharedServices].apiKey=GAODE_APPKEY;
+    [AMapSearchServices sharedServices].apiKey=GAODE_APPKEY;
+    
+    
+
+    
     /////////////////////////////////////////////////////////////
     /////////////////业务逻辑/////////////////////////////////////
     
@@ -63,7 +76,7 @@ static NSString *const AppKey = @"UwgavmLDCILH6xr6P7gXob8J";
         //跳转到主业务逻辑页面
         [self setupMainViewController];
 #warning for test
-        [self.window setRootViewController:[[UINavigationController alloc]initWithRootViewController:[LTPostListViewController new]]];
+        //[self.window setRootViewController:[[UINavigationController alloc]initWithRootViewController:[LTPostListViewController new]]];
     }
     [self.window makeKeyAndVisible];
     
