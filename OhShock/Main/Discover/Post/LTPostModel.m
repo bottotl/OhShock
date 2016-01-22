098079fd96d8ee1cd988048abd1d236596194386
bottotl <377632523@qq.com>
2016-01-22 19:27:41 +0800

    【postViewController 优化中】

---
+++ OhShock/Main/Discover/Post/LTPostModel.m
@@ -0,0 +1,13 @@
+//
+//  LTPostModel.m
+//  OhShock
+//
+//  Created by lintao.yu on 16/1/22.
+//  Copyright © 2016年 Lintao Yu. All rights reserved.
+//
+
+#import "LTPostModel.h"
+
+@implementation LTPostModel
+
+@end
