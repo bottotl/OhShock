5d23e302fbf0e45292d015bd8914aa359aaa77bc
hzhj <d245522099@163.com>
2016-01-31 22:57:10 +0800

    CDUserFactory有bug

---
+++ OhShock/Main/Group/Service/Chat/CDUserFactory.h
@@ -0,0 +1,15 @@
+//
+//  CDUserFactory.h
+//  OhShock
+//
+//  Created by chenlong on 16/1/31.
+//  Copyright © 2016年 Lintao Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "LeanChatLib.h"
+
+
+@interface CDUserFactory : NSObject
+
+@end
---
+++ OhShock/Main/Group/Service/Chat/CDUserFactory.m
@@ -0,0 +1,44 @@
+//
+//  CDUserFactory.m
+//  OhShock
+//
+//  Created by chenlong on 16/1/31.
+//  Copyright © 2016年 Lintao Yu. All rights reserved.
+//
+
+#import "CDUserFactory.h"
+//#import "LeanChatLib.h"
+
+//@interface CDUser : NSObject <CDUserModel>
+//
+//@property (nonatomic, strong) NSString *userId;
+//
+//@property (nonatomic, strong) NSString *username;
+//
+//@property (nonatomic, strong) NSString *avatarUrl;
+//
+//@end
+//
+//@implementation CDUser
+
+//@end
+
+
+@implementation CDUserFactory
+
+//#pragma mark - CDUserDelegate
+//
+//// cache users that will be use in getUserById
+//- (void)cacheUserByIds:(NSSet *)userIds block:(AVBooleanResultBlock)block {
+//    block(YES, nil); // don't forget it
+//}
+//
+//- (id <CDUserModelDelegate> )getUserById:(NSString *)userId {
+//    CDUser *user = [[CDUser alloc] init];
+//    user.userId = userId;
+//    user.username = userId;
+//    user.avatarUrl = @"http://ac-x3o016bx.clouddn.com/86O7RAPx2BtTW5zgZTPGNwH9RZD5vNDtPm1YbIcu";
+//    return user;
+//}
+
+@end
